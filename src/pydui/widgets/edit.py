06c5b4a30e4b586ef8c53adf3aa02d18148b4d5b
dzhsurf <dzhsurf@gmail.com>
2022-10-02 20:05:05 -0700

    Add Edit widget

--- src/pydui/widgets/edit.py
+++ src/pydui/widgets/edit.py
@@ -1,9 +1,16 @@
 # -*- coding: utf-8 -*-
+from pydui.core.base import PyDuiLayoutConstraint
+from pydui.core.gtk_widget_interface import PyDuiGtkWidgetInterface
+from pydui.core.import_gtk import *
 from pydui.core.widget import PyDuiWidget
 from pydui.widgets.pgview import PyDuiPGView
 
 
-class PyDuiEdit(PyDuiPGView):
+class PyDuiEdit(PyDuiPGView, PyDuiGtkWidgetInterface):
+
+    __gtk_text_view: Gtk.TextView = None
+    __gtk_scrolled_window: Gtk.ScrolledWindow = None
+
     @staticmethod
     def build_name() -> str:
         return "Edit"
@@ -11,5 +18,29 @@ class PyDuiEdit(PyDuiPGView):
     def __init__(self, parent: PyDuiWidget):
         super().__init__(parent)
 
+    def on_post_init(self):
+        self.__init_gtk_text_view_if_needed__()
+
     def parse_attrib(self, k: str, v: str):
         return super().parse_attrib(k, v)
+
+    def layout(self, x: float, y: float, width: float, height: float, constraint: PyDuiLayoutConstraint):
+        super().layout(x, y, width, height, constraint)
+        self.get_render_manager().move_gtk_widget(self, x, y)
+        self.__gtk_scrolled_window.set_size_request(width, height)
+
+    # PyDuiGtkWidgetInterface
+    def get_gtk_widget(self) -> Gtk.Widget:
+        return self.__gtk_scrolled_window
+
+    # private
+    def __init_gtk_text_view_if_needed__(self):
+        if self.__gtk_text_view is not None:
+            return
+        self.__gtk_scrolled_window = Gtk.ScrolledWindow()
+        self.__gtk_scrolled_window.set_hexpand(True)
+        self.__gtk_scrolled_window.set_vexpand(True)
+
+        self.__gtk_text_view = Gtk.TextView()
+        self.__gtk_scrolled_window.add(self.__gtk_text_view)
+        self.get_render_manager().put_gtk_widget(self)
