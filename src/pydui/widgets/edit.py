f00ad0813bbe6d71b9af952942a6b81ca2d5fb36
dzhsurf <dzhsurf@gmail.com>
2022-10-02 21:26:41 -0700

    Implement edit font, fontsize, text_padding attributes.

--- src/pydui/widgets/edit.py
+++ src/pydui/widgets/edit.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
+from typing import Tuple
+
+from pydui import utils
 from pydui.core.base import PyDuiLayoutConstraint
 from pydui.core.gtk_widget_interface import PyDuiGtkWidgetInterface
 from pydui.core.import_gtk import *
@@ -14,6 +17,7 @@ class PyDuiEdit(PyDuiPGView, PyDuiGtkWidgetInterface):
     __editable: bool = True  # Default is can edit
     __font: str = ""
     __fontsize: int = 0
+    __text_padding: Tuple[float, float, float, float] = (0, 0, 0, 0)
 
     @staticmethod
     def build_name() -> str:
@@ -31,17 +35,21 @@ class PyDuiEdit(PyDuiPGView, PyDuiGtkWidgetInterface):
         elif k == "editable":
             self.editable = v == "true"
         elif k == "font":
-            self.font = v
+            self.set_font(v)
         elif k == "fontsize":
-            self.fontsize = int(v)
+            self.set_fontsize(int(v))
+        elif k == "text_padding":
+            self.set_textpadding(utils.Str2Rect(v))
         super().parse_attrib(k, v)
 
     def layout(self, x: float, y: float, width: float, height: float, constraint: PyDuiLayoutConstraint):
         super().layout(x, y, width, height, constraint)
         if self.__gtk_scrolled_window is None:
             return
-        self.get_render_manager().move_gtk_widget(self, x, y)
-        self.__gtk_scrolled_window.set_size_request(width, height)
+        self.get_render_manager().move_gtk_widget(self, x + self.__text_padding[0], y + self.__text_padding[1])
+        text_padding_w = utils.RectW(self.__text_padding)
+        text_padding_h = utils.RectH(self.__text_padding)
+        self.__gtk_scrolled_window.set_size_request(max(0, width - text_padding_w), max(0, height - text_padding_h))
 
     # PyDuiGtkWidgetInterface
     def get_gtk_widget(self) -> Gtk.Widget:
@@ -73,33 +81,38 @@ class PyDuiEdit(PyDuiPGView, PyDuiGtkWidgetInterface):
             return
         self.__gtk_text_view.set_editable(self.__editable)
 
-    @property
-    def font(self) -> str:
+    def get_font(self) -> str:
         if self.__font == "":
             return self.get_render_manager().default_fontfamily
         return self.__font
 
-    @font.setter
-    def font(self, font: str):
+    def set_font(self, font: str):
         self.__font = font
         if self.__gtk_text_view is None:
             return
         desc = f"{self.font} {self.fontsize}"
         self.__gtk_text_view.override_font(Pango.font_description_from_string(desc))
 
-    @property
-    def fontsize(self) -> int:
+    def get_fontsize(self) -> int:
         if self.__fontsize == 0:
             return self.get_render_manager().default_fontsize
         return self.__fontsize
 
-    @fontsize.setter
-    def fontsize(self, fontsize: int):
+    def set_fontsize(self, fontsize: int):
         self.__fontsize = fontsize
         if self.__gtk_text_view is None:
             return
-        desc = f"{self.font} {self.fontsize}"
+        desc = f"{self.get_font()} {self.get_fontsize()}"
         self.__gtk_text_view.override_font(Pango.font_description_from_string(desc))
+
+    def set_textpadding(self, text_padding: Tuple[float, float, float, float]):
+        self.__text_padding = text_padding
+        if self.__gtk_text_view is None:
+            return
+        self.get_render_manager().notify_redraw()
+
+    def get_textpadding(self) -> Tuple[float, float, float, float]:
+        return self.__text_padding
 
     # private
     def __init_gtk_text_view_if_needed__(self):
@@ -110,7 +123,8 @@ class PyDuiEdit(PyDuiPGView, PyDuiGtkWidgetInterface):
         self.__gtk_scrolled_window.set_vexpand(True)
 
         self.__gtk_text_view = Gtk.TextView()
-        self.__gtk_text_view.override_font(Pango.font_description_from_string(f"{self.font} {self.fontsize}"))
+        desc = f"{self.get_font()} {self.get_fontsize()}"
+        self.__gtk_text_view.override_font(Pango.font_description_from_string(desc))
         self.__gtk_text_view.set_editable(self.__editable)
         self.__gtk_text_view.get_buffer().set_text(self.__text)
         self.__gtk_scrolled_window.add(self.__gtk_text_view)
